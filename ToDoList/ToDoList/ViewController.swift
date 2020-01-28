f485c060bfea2eacf737864993b15238e3465a63
gabrielespinosa936 <gabriel.espinosa936@myci.csuci.edu>
2020-01-27 22:45:44 -0800

    The start of my todo list app

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Gabriel Espinosa on 1/27/20.
+//  Copyright © 2020 Gabriel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
